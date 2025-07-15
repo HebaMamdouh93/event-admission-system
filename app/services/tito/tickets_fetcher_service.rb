412541a7d3338e1e3d856b174026afa3f1287dbc
Heba Mamdouh <hebamamdouh2016@gmail.com>
2025-07-15 22:18:42 +0300

    update tickets_fetcher_service to return array of ticket hashes and meta data

--- app/services/tito/tickets_fetcher_service.rb
+++ app/services/tito/tickets_fetcher_service.rb
@@ -7,7 +7,7 @@ module Tito
     def call
       client = ApiClient.new
       response = client.list_tickets(page: @page)
-      response.parsed_response["tickets"]
+      [ response.parsed_response["tickets"], response.parsed_response["meta"] ]
     end
   end
 end
--- spec/services/tito/tickets_fetcher_service_spec.rb
+++ spec/services/tito/tickets_fetcher_service_spec.rb
@@ -6,15 +6,15 @@ RSpec.describe Tito::TicketsFetcherService, :vcr do
       it "returns an array of ticket hashes" do
         result = described_class.new(page: 1).call
 
-        expect(result).to be_an(Array)
-        expect(result.first).to include("id", "email")
+        expect(result[0]).to be_an(Array)
+        expect(result[0].first).to include("id", "email")
       end
     end
 
     context "when there are no tickets on a high page number" do
       it "returns an empty array" do
         result = described_class.new(page: 999).call
-        expect(result).to eq([])
+        expect(result[0]).to eq([])
       end
     end
   end
