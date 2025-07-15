384af09c0107bc0ce543ca4dc73af2e51962e43a
Heba Mamdouh <hebamamdouh2016@gmail.com>
2025-07-15 18:57:27 +0300

    Add spec tests for TicketsFetcherService

---
+++ spec/services/tito/tickets_fetcher_service_spec.rb
@@ -0,0 +1,21 @@
+require "rails_helper"
+
+RSpec.describe Tito::TicketsFetcherService, :vcr do
+  describe "#call" do
+    context "when tickets exist on the given page" do
+      it "returns an array of ticket hashes" do
+        result = described_class.new(page: 1).call
+
+        expect(result).to be_an(Array)
+        expect(result.first).to include("id", "email")
+      end
+    end
+
+    context "when there are no tickets on a high page number" do
+      it "returns an empty array" do
+        result = described_class.new(page: 999).call
+        expect(result).to eq([])
+      end
+    end
+  end
+end
