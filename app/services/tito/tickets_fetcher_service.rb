54ea21971f8fe214c588fee2c562fa7bd44244b0
Heba Mamdouh <hebamamdouh2016@gmail.com>
2025-07-19 00:10:59 +0300

    Pass search parameter to Tito API

--- app/services/tito/tickets_fetcher_service.rb
+++ app/services/tito/tickets_fetcher_service.rb
@@ -2,11 +2,12 @@ module Tito
   class TicketsFetcherService
     def initialize(page: 1, search: {})
       @page = page
+      @search = search
     end
 
     def call
       client = ApiClient.new
-      response = client.list_tickets(page: @page)
+      response = client.list_tickets(page: @page, search: @search)
       [ response.parsed_response["tickets"], response.parsed_response["meta"] ]
     end
   end
