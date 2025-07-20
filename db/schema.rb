7006c8777ddbc84baf3b13a804b1ac1f40a6a95e
Heba Mamdouh <hebamamdouh2016@gmail.com>
2025-07-20 16:00:18 +0300

    Add admin migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_07_16_131045) do
+ActiveRecord::Schema[8.0].define(version: 2025_07_19_223125) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
+
+  create_table "admins", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_admins_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_admins_on_reset_password_token", unique: true
+  end
 
   create_table "jwt_denylists", force: :cascade do |t|
     t.string "jti"
@@ -34,6 +46,8 @@ ActiveRecord::Schema[8.0].define(version: 2025_07_16_131045) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "user_id"
+    t.datetime "deleted_at"
+    t.index ["deleted_at"], name: "index_tickets_on_deleted_at"
     t.index ["user_id"], name: "index_tickets_on_user_id"
   end
 
